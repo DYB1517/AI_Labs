e2006c72d17e528cd75398730f92bc0db05b0e98
DYB <62696621+DYB1517@users.noreply.github.com>
2025-11-21 15:13:20 +0300

    Lab_1 update

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -11,8 +11,8 @@ mtx = df.isnull()
 print(mtx.sum())
 
 vect_mod = ['HomePlanet', 'Cabin', 'Destination']
-vect_med = ['Age',  'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
-vect_mean = ['VIP','CryoSleep']
+vect_med = ['Age',  'RoomService',  'ShoppingMall', 'Spa', 'VRDeck']
+vect_mean = ['VIP','CryoSleep', 'FoodCourt']
 
 for col in vect_mod:
     if col:
@@ -25,18 +25,21 @@ for col in vect_mean:
     df[col] = df[col].fillna(df[col].mean())
     df[col] = df[col].astype(bool)
 
-df['VIP'] = df['VIP'].astype(int)
+vect_bti = ['VIP', 'CryoSleep', 'Transported']
+for col in vect_bti:
+    df[col] = df[col].astype(int)
 
 scaler = MinMaxScaler()
 
-vect_sc = ['RoomService','FoodCourt','ShoppingMall', 'Spa']
+vect_sc = ['Age', 'RoomService','FoodCourt','ShoppingMall', 'Spa']
 
 for col in vect_sc:
     df[col] = scaler.fit_transform(df[[col]])
-df = pd.get_dummies(df, columns=['Destination', 'HomePlanet', 'Cabin'], drop_first=True)
+df = pd.get_dummies(df, columns=['Destination', 'HomePlanet'], drop_first=True)
 
 df = df.drop('PassengerId', axis='columns')
 df = df.drop('Name', axis='columns')
+df = df.drop('Cabin', axis='columns')
 
 ed_mtx = df.isnull()
 print(ed_mtx.sum())
